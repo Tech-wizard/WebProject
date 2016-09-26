890ea91e6ff88ae47acea0650a7e3fc99aa71f6b
Tech-wizard <876424077@qq.com>
2016-09-26 21:29:58 +0800

    变量名更科学

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-function print(x:number):void {
- var str : string  = "";  
-
-    for(var j=0; j<x; j++){
-        str="";
-       for(var k=0;k<x-j-1;k++){
-            str+=" ";
+function Print_Pyramid(totalLine: number):void {
+ for (var currentLine = 0; currentLine < totalLine; currentLine++) {
+        var singleLineStr = "";
+        for (var spaceCount = totalLine - currentLine; spaceCount > 0; spaceCount--) {
+            singleLineStr += " ";
         }
-       for(var i=0; i<2*j+1; i++){
-            str+="*";
+        for (var starCount = 0; starCount < (currentLine * 2) - 1; starCount++) {
+            singleLineStr += "*";
         }
-     console.log(str);  
+        console.log(singleLineStr);
     }
 }
 
 window.onload = () => {
-   print(5);
+  Print_Pyramid(5);
 };
